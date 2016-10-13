0f424ad93ec81a8090da335160590cd5f11016ca
Silvano Stecher <silvano.stecher@stud.hslu.ch>
2016-10-13 15:02:05 +0200

    Reentrant in Event.c hinzugefügt & Platform_Local.h Events eingeschaltet

--- INTRO_Common/Event.c
+++ INTRO_Common/Event.c
@@ -29,26 +29,45 @@ static EVNT_MemUnit EVNT_Events[((EVNT_NOF_EVENTS-1)/EVNT_MEM_UNIT_NOF_BITS)+1];
 
 void EVNT_SetEvent(EVNT_Handle event) {
   /* \todo Make it reentrant */
+  CS1_CriticalVariable();
+
+  CS1_EnterCritical();
   SET_EVENT(event);
+  CS1_ExitCritical();
 }
 
 void EVNT_ClearEvent(EVNT_Handle event) {
   /* \todo Make it reentrant */
+  CS1_CriticalVariable();
+
+  CS1_EnterCritical();
   CLR_EVENT(event);
+  CS1_ExitCritical();
 }
 
 bool EVNT_EventIsSet(EVNT_Handle event) {
   /* \todo Make it reentrant */
-  return GET_EVENT(event);
+	bool res;
+  CS1_CriticalVariable();
+
+  CS1_EnterCritical();
+  res = GET_EVENT(event);
+  CS1_ExitCritical();
+  return res;
+
 }
 
 bool EVNT_EventIsSetAutoClear(EVNT_Handle event) {
   bool res;
   /* \todo Make it reentrant */
+  CS1_CriticalVariable();
+
+  CS1_EnterCritical();
   res = GET_EVENT(event);
   if (res) {
     CLR_EVENT(event); /* automatically clear event */
   }
+  CS1_ExitCritical();
   return res;
 }
 
@@ -56,7 +75,8 @@ void EVNT_HandleEvent(void (*callback)(EVNT_Handle), bool clearEvent) {
    /* Handle the one with the highest priority. Zero is the event with the highest priority. */
    EVNT_Handle event;
    /* \todo Make it reentrant */
-
+   CS1_CriticalVariable();
+   CS1_EnterCritical();
    for (event=(EVNT_Handle)0; event<EVNT_NOF_EVENTS; event++) { /* does a test on every event */
      if (GET_EVENT(event)) { /* event present? */
        if (clearEvent) {
@@ -65,6 +85,7 @@ void EVNT_HandleEvent(void (*callback)(EVNT_Handle), bool clearEvent) {
        break; /* get out of loop */
      }
    }
+   CS1_ExitCritical();
    if (event != EVNT_NOF_EVENTS) {
      callback(event);
      /* Note: if the callback sets the event, we will get out of the loop.
