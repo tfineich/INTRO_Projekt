ef217ef33ced53e39dcae6666c151f99c59fa404
Silvano Stecher <silvano.stecher@stud.hslu.ch>
2016-10-13 16:31:43 +0200

    Timer eingestellt für Remote

--- Remote/Sources/main.c
+++ Remote/Sources/main.c
@@ -38,6 +38,9 @@
 #include "HF1.h"
 #include "CS1.h"
 #include "KIN1.h"
+#include "TI1.h"
+#include "TimerIntLdd1.h"
+#include "TU1.h"
 /* Including shared modules, which are used for whole project */
 #include "PE_Types.h"
 #include "PE_Error.h"
